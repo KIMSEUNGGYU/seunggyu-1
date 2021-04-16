5d06969de8ba222fbc10da949f1fa66007218a31
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-04-16 17:49:14 +0900

    [feat] toast ui 에서 이미지 처리

--- client/components/pages/Write.tsx
+++ client/components/pages/Write.tsx
@@ -1,11 +1,16 @@
 import TUIEditor from "@ui/editor/TUIEditor";
+import ImageUploaderImpl from "@services/image_uploader";
+const imageUploader = new ImageUploaderImpl();
 
 export default function WritePage() {
   return (
     <div>
       <h1>좋은 블로그 내용을 작성하자!! 🔥🔥👋</h1>
 
-      <TUIEditor onChange={(value) => console.log(value)} />
+      <TUIEditor
+        imageUploader={imageUploader}
+        onChange={(value) => console.log(value)}
+      />
     </div>
   );
 }
