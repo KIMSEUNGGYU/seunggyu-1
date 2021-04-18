3a89bdab3cd0db28e2f91a737485dbf6c692af25
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-04-18 19:44:59 +0900

    [feat] UI 수정 삭제 UI 기능 구현

--- client/components/UI/post/PostInfo.tsx
+++ client/components/UI/post/PostInfo.tsx
@@ -1,27 +1,38 @@
 import { FC } from "react";
 import styled from "@emotion/styled";
 import { theme } from "@theme/index";
-import { Typography } from "antd";
 import { PostData } from "@common/types";
+import { Typography, Button } from "antd";
 const { Text } = Typography;
 
 const PostInfo = styled.div`
   width: 100%;
+
+  & > div {
+    margin-top: 16px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  Button + Button {
+    margin-left: 10px;
+    align-self: center;
+  }
 `;
 
 const Title = styled(Text)`
+  display: block;
   font-size: 2em;
   font-weight: bold;
 `;
 
 const DateString = styled(Text)`
   color: ${theme.GREY_TEXT_COLOR};
-  margin-left: 1em;
   font-weight: bold;
+  align-self: center;
 `;
 
 const Tags = styled.ul`
-  margin-top: 8px;
   display: flex;
 `;
 
@@ -30,7 +41,6 @@ const Tag = styled.li`
   font-weight: bold;
   margin-right: 6px;
   font-weight: bold;
-  font-size: 16px;
 `;
 
 interface Props {
@@ -41,7 +51,15 @@ const Info: FC = ({ post }: Props) => {
   return (
     <PostInfo>
       <Title>{post.title}</Title>
-      <DateString type="secondary">{post.date}</DateString>
+      <div>
+        <DateString type="secondary">{post.date}</DateString>
+        {true && (
+          <div>
+            <Button size="small"> 수정 </Button>
+            <Button size="small"> 삭제 </Button>
+          </div>
+        )}
+      </div>
       <Tags>
         {post.tags.map((tag, idx) => (
           <Tag key={idx}>#{tag}</Tag>
