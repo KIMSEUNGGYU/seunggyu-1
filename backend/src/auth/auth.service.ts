b4176b4d6cf52117e2930bea1acf22fb7791a65a
kimseunggyu <dev.seunggyu@gmail.com>
2021-05-13 12:26:06 +0900

    [feat] 로그인 비밀번호 해시 검증

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare, hash } from 'bcrypt';
 
 import { UsersService } from '@users/users.service';
+import { HttpException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
-  async validateUser(userId: string, password: string): Promise<any> {
-    const user = await this.usersService.getByUserId(userId);
-
-    if (user && user.password === password) {
+  async validateUser(userId: string, pass: string): Promise<any> {
+    try {
+      const user = await this.usersService.getByUserId(userId);
+      await this.verifyPassword(pass, user.password);
       const { password, ...result } = user;
       return result;
+    } catch (error) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
     }
-    return null;
+  }
+
+  private async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+    const isPasswordMatch = await compare(plainTextPassword, hashedPassword);
+    if (!isPasswordMatch) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST);
+    }
   }
 
   async login(user: any) {
