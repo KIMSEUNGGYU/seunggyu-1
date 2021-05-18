be7b24128c08abc2364c2a9626c60aa34071e519
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-05-18 23:57:23 +0900

    [G-29] front -  login 기능 cloud 연동 (#33)
    * [refactor] login 기능 cloud 연동
    
    * [fix] Mixed Cntent Error fix
    
    * [fix] Mixed Cntent Error fix

--- client/src/components/layout/Login.tsx
+++ client/src/components/layout/Login.tsx
@@ -14,6 +14,8 @@ function LoginPage() {
           content="https://res.cloudinary.com/du4w00gvm/image/upload/v1619410321/main_image.png"
         />
         <meta name="og:description" content="안전한 로그인 / 관리자만 사용할 수 있습니다." />
+        {/* Mixed Content Error fix */}
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
       <Login />
     </>
