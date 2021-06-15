09cd584c29087869c30e2534ae5867b0e2c00bdc
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-06-15 16:32:05 +0900

    [fix] mobild 환경에서 post 페이지 화면 이슈 수정 (#45)

--- client/src/components/UI/post/Contents.tsx
+++ client/src/components/UI/post/Contents.tsx
@@ -1,10 +1,9 @@
 import styled from '@emotion/styled';
 import { PostData } from '@common/types';
-// import TUIViewer from 'src/components/UI/editor/TUIViewer';
+
+import TUIViewer from 'src/components/UI/editor/TUIViewer';
 import ReactMarkdown from 'react-markdown';
 import renderers from './renderers';
-import gfm from 'remark-gfm';
-import { css, Global } from '@emotion/react';
 
 interface Props {
   post: PostData;
@@ -14,7 +13,7 @@ function Contents({ post }: Props) {
   return (
     <TemporaryBox>
       {/* <TUIViewer contents={post.contents} /> */}
-      <ReactMarkdown children={post.contents} renderers={renderers} />
+      <ReactMarkDownWrapper children={post.contents} renderers={renderers} />
     </TemporaryBox>
   );
 }
@@ -22,6 +21,44 @@ function Contents({ post }: Props) {
 const TemporaryBox = styled.div`
   margin-top: 32px;
   margin-bottom: 64px;
+`;
+
+const ReactMarkDownWrapper = styled(ReactMarkdown)`
+  color: ${({ theme }) => theme.postTextColor};
+  width: 100%;
+  font-size: 16px;
+
+  h1 {
+    margin-top: 32px;
+    margin-bottom: 4px;
+    font-size: 30px;
+  }
+  h2 {
+    margin-top: 22.4px;
+    margin-bottom: 1px;
+    font-size: 24x;
+  }
+  h3 {
+    margin-top: 16px;
+    margin-bottom: 1px;
+    font-size: 20px;
+  }
+  li {
+    list-style: disc;
+    margin-left: 1.7rem;
+    padding: 3px 2px;
+  }
+  p {
+    margin: 0;
+    padding: 3px 2px;
+  }
+
+  & img {
+    display: block;
+    margin: 0 auto;
+    padding: 1em 0;
+    max-width: 100%;
+  }
 `;
 
 export default Contents;
